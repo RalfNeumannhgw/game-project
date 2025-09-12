6efb967c2f6864f1ea127c13571fd775c923dc68
Ralf Neumann <Ralf.Neumann@dci-student.org>
2025-09-12 08:57:10 +0200

    202509120857

--- game-project/src/js/main.ts
+++ game-project/src/js/main.ts
@@ -3,7 +3,7 @@ import '../css/style.css';
 const clickMeButton = document.querySelector("#btn-start");
 const homeSection = document.querySelector("#home");
 const gameSection = document.querySelector("#game");
-console.log(clickMeButton);
+/* console.log(clickMeButton); */
 
 clickMeButton.addEventListener('click', function () {
     homeSection.style.display = "none";
@@ -46,32 +46,89 @@ User gewinnt, wenn nicht unentschieden und:
 5 <--> 3 || 4
 
 */
-let gameNum = 1;
-let matchNum = 1;
 
-function changeCounter(gameNum, matchNum) {
-    if (gameNum === 1) {
-        gameNum += 1
-    } else {
-        if (matchNum = 1) {
-            matchNum += 1
-        } else {
-            gameNum = 0;
-            matchNum = 0;
-            console.log("alles wieder 0")
-        }
-    }
-    console.log("game:" + gameNum, "match:" + matchNum)
+let userNum = {
+    gameUserNum: 0,
+    matchUserNum: 0
 }
 
+let compNum = {
+    gameCompNum: 0,
+    matchCompNum: 0
+}
+
+function changeCounter(object) {
+    if (object === userNum) {
+        if (object.gameUserNum < 1) {
+            object.gameUserNum += 1;
+        } else {
+            if (object.matchUserNum < 1) {
+                object.matchUserNum += 1;
+                object.gameUserNum = 0;
+                return object;
+            } else {
+                object.gameUserNum = 0;
+                object.matchUserNum = 0;
+                console.log("User hat das Spiel gewonnen")
+                return object;
+            }
+        }
+    } else {
+        if (object.gameCompNum < 1) {
+            object.gameCompNum += 1;
+        } else {
+            if (object.matchCompNum < 1) {
+                object.matchCompNum += 1;
+                object.gameCompNum = 0;
+                return object;
+            } else {
+                object.gameCompNum = 0;
+                object.matchCompNum = 0;
+                console.log("Computer hat das Spiel gewonnen");
+                return object;
+            }
+        }
+    }
+    return object;
+}
+
+function createInfo(choice, randomNum) {
+    if ((choice === 1 && randomNum === 2) || (choice === 2 && randomNum === 1)) {
+        console.log("Stein macht die Schere stumpf.")
+    }
+    if ((choice === 1 && randomNum === 5) || (choice === 5 && randomNum === 1)) {
+        console.log("Stein zerquetscht Echse.")
+    }
+    if ((choice === 2 && randomNum === 3) || (choice === 3 && randomNum === 2)) {
+        console.log("Schere zerschneidet Papier.")
+    }
+    if ((choice === 2 && randomNum === 5) || (choice === 5 && randomNum === 2)) {
+        console.log("Schere köpft die Echse.")
+    }
+    if ((choice === 3 && randomNum === 1) || (choice === 1 && randomNum === 3)) {
+        console.log("Papier wickelt den Stein ein.")
+    }
+    if ((choice === 3 && randomNum === 4) || (choice === 4 && randomNum === 3)) {
+        console.log("Papier widerlegt Spock.")
+    }
+    if ((choice === 4 && randomNum === 1) || (choice === 1 && randomNum === 4)) {
+        console.log("Spock verdampft Stein.")
+    }
+    if ((choice === 4 && randomNum === 2) || (choice === 2 && randomNum === 4)) {
+        console.log("Spock zertrümmert Schere.")
+    }
+    if ((choice === 5 && randomNum === 3) || (choice === 3 && randomNum === 5)) {
+        console.log("Echse frisst Papier.")
+    }
+    if ((choice === 5 && randomNum === 4) || (choice === 4 && randomNum === 5)) {
+        console.log("Echse vergiftet Spock")
+    }
+}
 
 function handleChoice(choiceValue: number) {
     choice = choiceValue;
     if (choice === randomNum) {
-        console.log("Unentschieden")
-        console.log("Unentschieden: " + gameNum, matchNum);
-        changeCounter(gameNum, matchNum);
-        console.log("Unentschieden: " + gameNum, matchNum);
+        console.log("Unentschieden !!!");
         randomNum = randomSymbol();
 
     } else if (
@@ -81,17 +138,21 @@ function handleChoice(choiceValue: number) {
         (choice === 4 && (randomNum === 1 || randomNum === 2)) ||
         (choice === 5 && (randomNum === 3 || randomNum === 4))
     ) {
+        createInfo(choice, randomNum);
+
         console.log("User gewinnt!")
-        console.log("User gewinnt: " + gameNum, matchNum);
-        changeCounter(gameNum, matchNum);
-        console.log("User gewinnt: " + gameNum, matchNum);
+        console.log("User gewinnt: " + userNum.gameUserNum, userNum.matchUserNum);
+        userNum = changeCounter(userNum);
+        console.log("User gewinnt: " + userNum.gameUserNum, userNum.matchUserNum);
         randomNum = randomSymbol();
 
     } else {
+        createInfo(choice, randomNum);
+
         console.log("Computer gewinnt!");
-        console.log("Computer gewinnt: "+ gameNum, matchNum);
-        changeCounter(gameNum, matchNum);
-        console.log("Computer gewinnt: " + gameNum, matchNum);
+        console.log("Computer gewinnt: " + compNum.gameCompNum, compNum.matchCompNum);
+        compNum = changeCounter(compNum);
+        console.log("Computer gewinnt: " + compNum.gameCompNum, compNum.matchCompNum);
         randomNum = randomSymbol();
 
     }
